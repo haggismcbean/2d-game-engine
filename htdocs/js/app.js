7a5aa4459c99a44bc56b9744b794ea9605c1fc0b
Harry Bishop <hbishop11@gmail.com>
2016-01-17 13:45:43 +0000

    requirejs classes setup

--- htdocs/js/app.js
+++ htdocs/js/app.js
@@ -1,7 +1,12 @@
 requirejs.config({
 	"baseUrl": "js",
 	"paths": {
+		// Libraries
 		"Class": "libs/class",
-		"Jquery": "libs/jquery"
+		"Jquery": "libs/jquery",
+		// Classes
+		"Launcher": "app/classes/Launcher" 
 	}
-})
+});
+
+require(['app/main']);
